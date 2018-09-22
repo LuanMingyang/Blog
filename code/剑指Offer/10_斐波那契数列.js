82fc20bda43bba39f5cc383acd57b171a8843930
LuanMingyang <982684249@qq.com>
2018-09-22 11:23:45 +0800

    剑指

--- code/剑指Offer/10_斐波那契数列.js
+++ code/剑指Offer/10_斐波那契数列.js
@@ -1,8 +1,8 @@
 /**
  * 斐波那契数列定义：
- *           0                     n = 0
- * f(n) = ｛ 1                     n = 1
- *           f(n - 1) + f(n - 2)   n > 1
+ *           0                 n = 0
+ * f(n) = ｛ 1                 n = 1
+ *           f(n-1) + f(n-2)   n > 1
  * 
  * 题目一：求斐波那契数列的第 n 项
  * 解析：
@@ -18,10 +18,17 @@
  * 先考虑简单情况，只有1级台阶，只有一种跳法；有2级台阶，可分两次跳，每次跳1级，或者一次跳2级。
  * 一般情况，把 n 级台阶时的跳法看成n的函数，记为 f(n)；
  * 当 n > 2 时，每一次跳时有两种不同的选择：
- * 1) 只跳1级，此时跳法数目等于后面剩下的 n - 1 级台阶的跳法数目，即 f(n - 1);
- * 2) 跳2级，此时跳法数目等于后面剩下的 n - 2 级台阶的跳法数目，即 f(n - 2);
- * 因此，n 级台阶的不同跳法总数 f(n) = f(n - 1) + f(n - 2)
+ * 1) 只跳1级，此时跳法数目等于后面剩下的 n-1 级台阶的跳法数目，即 f(n-1);
+ * 2) 跳2级，此时跳法数目等于后面剩下的 n-2 级台阶的跳法数目，即 f(n-2);
+ * 因此，n 级台阶的不同跳法总数 f(n) = f(n-1) + f(n-2)
  * 实际上就是斐波那契数列
+ * 
+ * 题目二变形：变态跳台阶
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+ * 求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ * n 级台阶的不同跳法总数     f(n) = f(n-1) + f(n-2) + f(n-3) + ... + f(1)   ①
+ * n-1 级台阶的不同跳法总数 f(n-1) =          f(n-2) + f(n-3) + ... + f(1)   ②
+ * ① - ②得：f(n) - f(n-1) = f(n-1) 即 f(n) = 2f(n-1)
  * 
  * 题目三：使用 2×1 的小矩阵横着或者竖着去覆盖更大的矩阵。
  * 请问用 8 个 2×1 的小矩阵无重叠的覆盖一个 2×8 的大矩阵，总共有多少种方法？
@@ -46,6 +53,53 @@ function fibonacci(n) {
   let fibNMinus2 = 0;
   let fibN = 0;
   for (let i = 2; i <= n; i++) {
+    fibN = fibNMinus1 + fibNMinus2;
+    fibNMinus2 = fibNMinus1;
+    fibNMinus1 = fibN;
+  }
+  return fibN;
+}
+
+// 跳台阶
+function jumpFloor(number)
+{
+  if (number <= 2) {
+    return number;
+  }
+  let fibNMinus1 = 2;
+  let fibNMinus2 = 1;
+  let fibN = 0;
+  for (let i = 3; i <= number; i++) {
+    fibN = fibNMinus1 + fibNMinus2;
+    fibNMinus2 = fibNMinus1;
+    fibNMinus1 = fibN;
+  }
+  return fibN;
+}
+
+// 变态跳台阶
+function jumpFloorII(number)
+{
+  if (number <= 1) {
+    return number;
+  }
+  let fibN = 1;
+  for (let i = 2; i <= number; i++) {
+    fibN *= 2;
+  }
+  return fibN;
+}
+
+// 矩形覆盖
+function rectCover(number)
+{
+  if (number <= 2) {
+    return number;
+  }
+  let fibNMinus1 = 2;
+  let fibNMinus2 = 1;
+  let fibN = 0;
+  for (let i = 3; i <= number; i++) {
     fibN = fibNMinus1 + fibNMinus2;
     fibNMinus2 = fibNMinus1;
     fibNMinus1 = fibN;
